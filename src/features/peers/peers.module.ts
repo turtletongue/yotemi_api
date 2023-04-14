e7d59bdc160783f9d021f1838b264b9f94f91dc0
turtletongue <platon.s14@list.ru>
2023-04-14 19:27:41 +0500

    fix: use interviews module in peers module

--- src/features/peers/peers.module.ts
+++ src/features/peers/peers.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import IdentifiersModule from '@common/identifiers';
 import GatewaysModule from '@common/gateways';
+import InterviewsModule from '@features/interviews';
 import PeersService from './controllers/peers.service';
 import PeersController from './controllers/peers.controller';
 import PeersGateway from './peers.gateway';
 
 @Module({
-  imports: [IdentifiersModule, GatewaysModule],
+  imports: [IdentifiersModule, GatewaysModule, InterviewsModule],
   controllers: [PeersController],
   providers: [PeersService, PeersGateway],
 })
