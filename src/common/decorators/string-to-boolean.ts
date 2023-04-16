42ca378631f07b84feefedc926c35f0fe7445f26
turtletongue <platon.s14@list.ru>
2023-04-16 19:07:17 +0500

    debug

--- src/common/decorators/string-to-boolean.ts
+++ src/common/decorators/string-to-boolean.ts
@@ -3,6 +3,9 @@ import { Transform } from 'class-transformer';
 import { booleanStringToBoolean } from '@common/utils';
 
 const StringToBoolean = () =>
-  Transform(({ value }) => booleanStringToBoolean[value]);
+  Transform(({ value }) => {
+    console.log('transform', value, booleanStringToBoolean[value]);
+    return booleanStringToBoolean[value];
+  });
 
 export default StringToBoolean;
